c3510b0d13e6645b4928e9ad3cb1d7fcdef1797b
TheAwesomeARay <andrewray4@gmail.com>
2019-03-16 14:28:20 -0500

    Initial

---
+++ MediatRSample/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace MediatRSample.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ MediatRSample/IoC/IoCConfig.cs
@@ -0,0 +1,35 @@
+﻿using Autofac;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MediatRSample.IoC
+{
+    public class IoCStartUp
+    {
+        public static void Configure()
+        {
+            var builder = new ContainerBuilder();
+
+            builder
+              .RegisterType<Mediator>()
+              .As<IMediator>()
+              .InstancePerLifetimeScope();
+
+            // request & notification handlers
+            builder.Register<ServiceFactory>(context =>
+            {
+                var c = context.Resolve<IComponentContext>();
+                return t => c.Resolve(t);
+            });
+
+            // finally register our custom code (individually, or via assembly scanning)
+            // - requests & handlers as transient, i.e. InstancePerDependency()
+            // - pre/post-processors as scoped/per-request, i.e. InstancePerLifetimeScope()
+            // - behaviors as transient, i.e. InstancePerDependency()
+            //builder.RegisterAssemblyTypes(typeof(MyType).GetTypeInfo().Assembly).AsImplementedInterfaces();
+        }
+    }
+}
